43144f49570671f9f097b257a761e27ebcdc8d29
Christopher D. Long <galizur@gmail.com>
2015-08-08 20:55:34 -0400

    World Rugby update.

--- world_rugby/sos_zinb.sh
+++ world_rugby/sos_zinb.sh
@@ -25,3 +25,6 @@ cp /tmp/zinb_current_ranking.csv sos/zinb_current_ranking.csv
 
 psql rugby -f sos/zinb_predict_monthly.sql > sos/zinb_predict_monthly.txt
 cp /tmp/zinb_predict_monthly.csv sos/zinb_predict_monthly.csv
+
+psql rugby -f sos/zinb_predict.sql > sos/zinb_predict.txt
+cp /tmp/zinb_predict.csv sos/zinb_predict.csv
