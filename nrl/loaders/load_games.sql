e9f278ddfac6535e780d38b61fd641d4c77148f4
Christopher D. Long <galizur@gmail.com>
2020-03-20 23:07:46 -0400

    National Rugby League and analytics; initial import.

---
+++ nrl/loaders/load_games.sql
@@ -0,0 +1,26 @@
+begin;
+
+drop table if exists nrl.games;
+
+set datestyle to 'SQL, DMY';
+
+create table nrl.games (
+	round			text,
+	date			timestamp,
+	location		text,
+	home_team		text,
+	away_team		text,
+	result			text,
+	home_score		integer,
+	away_score		integer
+);
+
+copy nrl.games from '/tmp/games.csv' with delimiter as ',' csv;
+
+alter table nrl.games add column game_id serial;
+
+update nrl.games
+set home_score = split_part(result,' - ',1)::integer,
+    away_score = split_part(result,' - ',2)::integer;
+
+commit;
