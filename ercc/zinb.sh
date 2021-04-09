612c723dfecf22006ed4023051285bde3a71c3ba
Christopher D. Long <galizur@gmail.com>
2021-04-09 02:08:20 -0400

    European Cups update.

--- ercc/zinb.sh
+++ ercc/zinb.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-psql rugby -c "drop table if exists premiership.results;"
+psql rugby -c "drop table if exists ercc.results;"
 
 psql rugby -f sos/standardized_results.sql
 
-psql rugby -c "vacuum full verbose analyze premiership.results;"
+psql rugby -c "vacuum full verbose analyze ercc.results;"
 
-psql rugby -c "drop table if exists premiership._zinb_basic_factors;"
-psql rugby -c "drop table if exists premiership._zinb_parameter_levels;"
+psql rugby -c "drop table if exists ercc._zinb_basic_factors;"
+psql rugby -c "drop table if exists ercc._zinb_parameter_levels;"
 
 #R --vanilla -f sos/zinb.R
 R -f sos/zinb.R
 
-psql rugby -c "vacuum full verbose analyze premiership._zinb_parameter_levels;"
-psql rugby -c "vacuum full verbose analyze premiership._zinb_basic_factors;"
+psql rugby -c "vacuum full verbose analyze ercc._zinb_parameter_levels;"
+psql rugby -c "vacuum full verbose analyze ercc._zinb_basic_factors;"
 
 psql rugby -f sos/zinb_normalize_factors.sql
-psql rugby -c "vacuum full verbose analyze premiership._zinb_factors;"
+psql rugby -c "vacuum full verbose analyze ercc._zinb_factors;"
 
 psql rugby -f sos/zinb_schedule_factors.sql
-psql rugby -c "vacuum full verbose analyze premiership._zinb_schedule_factors;"
+psql rugby -c "vacuum full verbose analyze ercc._zinb_schedule_factors;"
 
 psql rugby -f sos/zinb_current_ranking.sql > sos/zinb_current_ranking.txt
 cp /tmp/zinb_current_ranking.csv sos/zinb_current_ranking.csv
