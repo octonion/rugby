512652f10bf0a5d6f9f953af8754e88a54697c88
Christopher D. Long <galizur@gmail.com>
2021-05-27 01:41:44 -0400

    Major League Rugby update.

--- major_league/scripts/sos.sh
+++ major_league/scripts/sos.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-psql rugby -c "drop table if exists ercc.results;"
+psql rugby -c "drop table if exists major_league.results;"
 
 psql rugby -f sos/standardized_results.sql
 
-psql rugby -c "vacuum full verbose analyze ercc.results;"
+psql rugby -c "vacuum full verbose analyze major_league.results;"
 
-psql rugby -c "drop table if exists ercc._basic_factors;"
-psql rugby -c "drop table if exists ercc._parameter_levels;"
+psql rugby -c "drop table if exists major_league._basic_factors;"
+psql rugby -c "drop table if exists major_league._parameter_levels;"
 
 R --vanilla -f sos/lmer.R
 
-psql rugby -c "vacuum full verbose analyze ercc._parameter_levels;"
-psql rugby -c "vacuum full verbose analyze ercc._basic_factors;"
+psql rugby -c "vacuum full verbose analyze major_league._parameter_levels;"
+psql rugby -c "vacuum full verbose analyze major_league._basic_factors;"
 
 psql rugby -f sos/normalize_factors.sql
-psql rugby -c "vacuum full verbose analyze ercc._factors;"
+psql rugby -c "vacuum full verbose analyze major_league._factors;"
 
 psql rugby -f sos/schedule_factors.sql
-psql rugby -c "vacuum full verbose analyze ercc._schedule_factors;"
+psql rugby -c "vacuum full verbose analyze major_league._schedule_factors;"
 
 psql rugby -f sos/current_ranking.sql > sos/current_ranking.txt
 cp /tmp/current_ranking.csv sos/current_ranking.csv
