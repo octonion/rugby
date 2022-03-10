f05faff79132b6522e595641ce984c3d3fe06777
Christopher D. Long <galizur@gmail.com>
2022-03-09 21:40:55 -0500

    NRL update for 2022.

--- nrl/pull_update.sh
+++ nrl/pull_update.sh
@@ -1,3 +1,4 @@
 cd csv
-wget -N https://fixturedownload.com/download/nrl-2021-AUSEasternStandardTime.csv
+wget -N https://fixturedownload.com/download/nrl-2022-UTC.csv
+#wget -N https://fixturedownload.com/download/nrl-2021-AUSEasternStandardTime.csv
 cd ..
