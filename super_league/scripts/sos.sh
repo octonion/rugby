47d1a48c36dabcc532d1f781a840c80433aa3e7d
Christopher D. Long <galizur@gmail.com>
2022-04-29 00:34:20 -0400

    Super League update.

--- super_league/scripts/sos.sh
+++ super_league/scripts/sos.sh
@@ -9,7 +9,7 @@ psql rugby -c "vacuum full verbose analyze sl.results;"
 psql rugby -c "drop table if exists sl._basic_factors;"
 psql rugby -c "drop table if exists sl._parameter_levels;"
 
-R --vanilla -f sos/lmer.R
+R -f sos/lmer.R
 
 psql rugby -c "vacuum full verbose analyze sl._parameter_levels;"
 psql rugby -c "vacuum full verbose analyze sl._basic_factors;"
