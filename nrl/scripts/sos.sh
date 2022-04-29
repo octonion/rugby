e71ffa5d16ee013f1fd69d1008e6cd2dad807d7d
Christopher D. Long <galizur@gmail.com>
2022-04-28 21:01:50 -0400

    NRL update.

--- nrl/scripts/sos.sh
+++ nrl/scripts/sos.sh
@@ -9,7 +9,7 @@ psql rugby -c "vacuum full verbose analyze nrl.results;"
 psql rugby -c "drop table if exists nrl._basic_factors;"
 psql rugby -c "drop table if exists nrl._parameter_levels;"
 
-R --vanilla -f sos/lmer.R
+R -f sos/lmer.R
 
 psql rugby -c "vacuum full verbose analyze nrl._parameter_levels;"
 psql rugby -c "vacuum full verbose analyze nrl._basic_factors;"
