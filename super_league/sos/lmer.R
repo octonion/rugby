e45a9f611e606b9cc11bac5e5ecbbf5cf79e6425
Christopher D. Long <galizur@gmail.com>
2022-07-09 00:28:36 -0400

    Super League update.

--- super_league/sos/lmer.R
+++ super_league/sos/lmer.R
@@ -16,11 +16,11 @@ r.field as field,
 r.team_name as team,
 r.opponent_name as opponent,
 team_score::float as gs,
-year-2020 as w
+year-2021 as w
 from sl.results r
 
 where
-    r.year between 2021 and 2022
+    r.year between 2022 and 2022
 
 ;")
 
